654416765a3801de121dd9dd22469886cbe6cea2
Hakan Bilgic <hbilgic1992@gmail.com>
2023-03-29 21:05:09 +0200

    custom effects

--- mouseevents/src/Experience.jsx
+++ mouseevents/src/Experience.jsx
@@ -8,11 +8,15 @@ import {
   Noise,
   Bloom,
   DepthOfField,
+  SSR,
 } from "@react-three/postprocessing";
 import { BlendFunction, GlitchEffect, GlitchMode } from "postprocessing";
+import { useControls } from "leva";
+import Drunk from "./Drunk";
 
 export default function Experience() {
   const cube = useRef();
+  const drunkRef = useRef();
 
   useFrame((state, delta) => {
     cube.current.rotation.y += delta * 0.2;
@@ -22,9 +26,45 @@ export default function Experience() {
     cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
   }
 
+  const ssrProps = useControls({
+    temporalResolve: true,
+    STRETCH_MISSED_RAYS: true,
+    USE_MRT: true,
+    USE_NORMALMAP: true,
+    USE_ROUGHNESSMAP: true,
+    ENABLE_JITTERING: true,
+    ENABLE_BLUR: true,
+    temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+    temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+    maxSamples: { value: 0, min: 0, max: 1 },
+    resolutionScale: { value: 1, min: 0, max: 1 },
+    blurMix: { value: 0.5, min: 0, max: 1 },
+    blurKernelSize: { value: 8, min: 0, max: 8 },
+    blurSharpness: { value: 0.5, min: 0, max: 1 },
+    rayStep: { value: 0.3, min: 0, max: 1 },
+    intensity: { value: 1, min: 0, max: 5 },
+    maxRoughness: { value: 0.1, min: 0, max: 1 },
+    jitter: { value: 0.7, min: 0, max: 5 },
+    jitterSpread: { value: 0.45, min: 0, max: 1 },
+    jitterRough: { value: 0.1, min: 0, max: 1 },
+    roughnessFadeOut: { value: 1, min: 0, max: 1 },
+    rayFadeOut: { value: 0, min: 0, max: 1 },
+    MAX_STEPS: { value: 20, min: 0, max: 20 },
+    NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+    maxDepthDifference: { value: 3, min: 0, max: 10 },
+    maxDepth: { value: 1, min: 0, max: 1 },
+    thickness: { value: 10, min: 0, max: 10 },
+    ior: { value: 1.45, min: 0, max: 2 },
+  });
+
+  const drunkProps = useControls("Drunk Effect", {
+    frequency: { value: 2, min: 0, max: 20, step: 0.01 },
+    amplitude: { value: 0.1, min: 0, max: 1 },
+  });
+
   return (
     <>
-      <color args={["#000"]} attach="background" />
+      <color args={["white"]} attach="background" />
       <EffectComposer>
         {/* <Vignette
           offset={0.3}
@@ -44,6 +84,8 @@ export default function Experience() {
           foucsLength={0.025}
           bokehScale={6}
         /> */}
+        {/* <SSR {...ssrProps} /> */}
+        <Drunk ref={drunkRef} {...drunkProps} />
       </EffectComposer>
 
       <OrbitControls makeDefault />
@@ -68,7 +110,7 @@ export default function Experience() {
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
+        <meshStandardMaterial color="black" />
       </mesh>
     </>
   );
