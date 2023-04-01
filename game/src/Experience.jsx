dcdde1e7925fa4154aadbc79ca8e27500c4d0e60
Hakan Bilgic <hbilgic1992@gmail.com>
2023-04-01 17:17:35 +0200

    add player, controls, camera and shadows

--- game/src/Experience.jsx
+++ game/src/Experience.jsx
@@ -1,18 +1,18 @@
-import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
 import { Level } from "./Level.jsx";
 import { Physics, Debug } from "@react-three/rapier";
-Physics;
+import Player from "./Player.jsx";
 
 export default function Experience() {
   return (
     <>
-      <OrbitControls makeDefault />
+      {/* <OrbitControls makeDefault /> */}
 
       <Physics>
-        <Debug />
+        {/* <Debug /> */}
         <Lights />
         <Level />
+        <Player />
       </Physics>
     </>
   );
